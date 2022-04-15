f2822078b8522b8a5297713b0d40b5e31a1cc29e
Brian Aston <astonb.dev@gmail.com>
2022-04-15 12:07:23 -0700

    Implemeneted bubble, tests pass:

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+//[4, 20, 12, 10, 7, 9]
+
+function bubbleSort(list) {
+  for (let i = list.length; i >= 1; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (list[j] > list[j + 1]) {
+        const temp = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = temp
+      }
+    }
+  }
+
+  return list
+}
 
 module.exports = bubbleSort;
